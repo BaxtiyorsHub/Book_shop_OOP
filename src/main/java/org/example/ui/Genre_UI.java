f8f91a80153f2bd7c7f60cd198fdb13d01ebec34
YusupovJavaDev <baxtiyorking17@gmail.com>
2025-03-20 19:10:49 +0500

    genre ui

--- src/main/java/org/example/ui/Genre_UI.java
+++ src/main/java/org/example/ui/Genre_UI.java
@@ -1,20 +1,49 @@
 package org.example.ui;
 
+import org.example.ScannerUtil.ScannerUtil;
 import org.example.entities.ProfileEntity;
 
 public class Genre_UI {
 
     public void start(ProfileEntity profile) {
+        while (true) {
+            switch (menu()){
+                case 1 -> genreCreate(profile);
+                case 2 -> genreUpdate(profile);
+                case 3 -> genreRead();
+                case 4 -> genreDelete(profile);
+                case 0 -> {return;}
+            }
+        }
+    }
+
+    private void genreDelete(ProfileEntity profile) {
 
     }
-    public int menu() {
-        System.out.println("*****GENRE*****");
-        System.out.println("1.Genre Create");
-        System.out.println("2.Genre Update");
-        System.out.println("3.Genre Read");
-        System.out.println("4.Genre Delete");
-        System.out.println("0.Exit");
-        System.out.print("Enter menu : ");
-        return ScannerUtil.SCANNER_NUM.nextInt();
+
+    private void genreRead() {
+
+    }
+
+    private void genreUpdate(ProfileEntity profile) {
+
+    }
+
+    private void genreCreate(ProfileEntity profile) {
+
+    }
+
+    private int menu() {
+        System.out.print("""
+                +------  MENU  ------+
+                | 1 | Genre Create   |
+                | 2 | Genre Update   |
+                | 3 | Genre Read     |
+                | 4 | Genre Delete   |
+                +--------------------+
+                | 0 | Back           |
+                +--------------------+
+                | Enter Option ->""");
+        return ScannerUtil.scanNUM.nextInt();
     }
 }
